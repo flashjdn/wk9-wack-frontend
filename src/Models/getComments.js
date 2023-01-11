ef02a0e30f70849d9f9b077a7ec498996daaeeb0
Jordan Flash <flash.jordan.3@gmail.com>
2023-01-11 16:07:48 +0000

    adding render endpoints

--- src/Models/getComments.js
+++ src/Models/getComments.js
@@ -1,5 +1,5 @@
 export const getComments = async (post_id) => {
-    const res = await fetch(`https://customer.elephantsql.com/api/instances`);
+    const res = await fetch(`https://wack.onrender.com/comments`);
     const data = await res.json();
     //   console.log(data);
   
--- src/Models/getPosts.js
+++ src/Models/getPosts.js
@@ -1,5 +1,5 @@
 export const getPosts = async () => {
-    const res = await fetch(`https://customer.elephantsql.com/api/instances`);
+    const res = await fetch(`https://wack.onrender.com/posts`);
     
     const data = await res.json();
     // console.log(data);
